5b27a653d30ae911a05251fb70561722e542acd2
k3mystra <programmer.coleslaw981@slmail.me>
2025-06-20 23:29:41 +0800

    feat: Add constructor for the Vehicle struct

---
+++ vehicle.m
@@ -0,0 +1,10 @@
+function v = vehicle(iat, fuelType, refuelQuantity)
+    v = struct(
+        'iat', iat,
+        'fuelType', fuelType,
+        'refuelQuantity', refuelQuantity,
+        'arrivalTime', 0,
+        'waitingDuration', 0,
+        'initialLineNumber', 0
+    );
+end
