9472539a4f0a9f847924298d49852619af6b4cfc
k3mystra <programmer.coleslaw981@slmail.me>
2025-06-20 23:39:37 +0800

    fix: structure array cannot be defined like that

--- vehicle.m
+++ vehicle.m
@@ -1,10 +1,3 @@
 function v = vehicle(iat, fuelType, refuelQuantity)
-    v = struct(
-        'iat', iat,
-        'fuelType', fuelType,
-        'refuelQuantity', refuelQuantity,
-        'arrivalTime', 0,
-        'waitingDuration', 0,
-        'initialLineNumber', 0
-    );
+    v = struct('iat', iat,'fuelType', fuelType,'refuelQuantity', refuelQuantity,'arrivalTime', 0,'waitingDuration', 0,'initialLineNumber', 0);
 end
