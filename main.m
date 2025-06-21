a2a40fb4167f61ee0baa798a0fa50add3761e507
JaxChoong <choongjax@gmail.com>
2025-06-21 15:34:23 +0800

    fix: changed the function call to be 3rd rng and also func call

--- main.m
+++ main.m
@@ -1,29 +1,8 @@
-user_input = input_system()
-
-% Process user input, generate random numbers accordingly, and generate a list of cars
-% Note that at this point the Car object must already have its appropriate value
-
-% cars = generate_car();
-
-% This is the format of a Car object. Replace zero with the appropriate value when constructing a Car
-car = struct(
-    'iat', 0,
-    'fuelType', 0,
-    'refuelDuration', 0,
-    'arrivalTime', 0,
-    'waitingDuration', 0,
-    'initialLineNumber', 0
-);
-
-simulate(cars);
-
-% Print cars data
-table_printing(cars);
 
 %   BELOW THIS IS THE RNG SHITEEEEE
 
 number_of_cars = 10;                     % Number of cars to generate, replace this with user input
-selected_prng = 2;                      % Selected PRNG, replace this with user input
+selected_prng = 3;                      % Selected PRNG, replace this with user input
 seed = rand();                          % randomise the seed
 
 randomised = zeros(number_of_cars,3);      % initialise matrix to hold car values
@@ -48,7 +27,7 @@ switch selected_prng
     case 2
         randomised = lcg(seed, number_of_cars);
     case 3
-        randomised = xorshiftrp(seed, number_of_cars);
+        randomised = xorshiftr(seed, number_of_cars);
     otherwise
         error('Invalid PRNG selection');
 end
