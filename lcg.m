d436194c3210f0a60997286e0bcc6668d32310e4
KHY0924 <keehuiyee0924@gmail.com>
2025-06-21 16:02:33 +0800

    Update comment

--- lcg.m
+++ lcg.m
@@ -1,20 +1,19 @@
 function g = lcg(seed, M)
 
-    a = 1664525;       % set multiplier 
-    c = 1013904223;    % set increment
-    m = 2^32;         % modulus to turn values into 32 bit
+    a = 1664525;        
+    c = 1013904223;  
+    m = 2^32;        
 
-    state = uint32(seed*2^32);
- 
+    state = uint32(seed*2^32);  %convert seed to 32 bit unsigned number 
 
-    g = zeros(M, 3);
+    g = zeros(M, 3);   %use to store random number
 
-    % Loop to fill the matrix with values
+    %Loop the number
     for i = 1:M*3
-            % Update the state using the LCG formula: state = (a * state + c) mod m
+            % change state to double and use formula = (a * state + c) mod m 
             state = mod(a * double(state) + c, m);
 
-            % Scale the generated value to the range [1, 100] and store it in the matrix
+            % become a integer between [0,100] and store in matrix
             g(i) = round(abs(state / m) * 99) + 1;  % Map to range [1, 100]
     end
 end
