69c4a4e41cb552d25f1cbdafe907887ad3271855
k3mystra <programmer.coleslaw981@slmail.me>
2025-06-22 10:27:30 +0800

    formatting formatting

--- main.m
+++ main.m
@@ -22,13 +22,13 @@ randomised = zeros(number_of_cars,3);      % initialise matrix to hold car value
 disp(randomised);
 % ARRAY STRUCTURE:
 % For each car we have 3 randomised values, so we will have 3 coloumns of N values in the randomised array
-% For N  number of cars, the first coloumn of the matrix will be for petrol type, 
+% For N  number of cars, the first coloumn of the matrix will be for petrol type,
 % the second coloumn of the matrix will be for interarrival
 % and the third coloumn will be for service time
 
-% eg: for 2 cars, 
+% eg: for 2 cars,
 % [1, 3, 5;
-%  2, 4, 6]  
+%  2, 4, 6]
 % Petrol type = [1,2]
 % Interarrival = [3,4]
 % Service time =[5,6]
@@ -45,20 +45,20 @@ switch selected_prng
 end
 disp(randomised)
 % initialise array to hold petrol type values
-petrol_type_rng = zeros(1, number_of_cars);  
+petrol_type_rng = zeros(1, number_of_cars);
 petrol_type_values = cell(1, number_of_cars);
-% fill petrol type rng array 
+% fill petrol type rng array
 for i = 1:number_of_cars
     petrol_type_rng(i) = randomised(i);
     petrol_type_values{i} = get_petrol_value(randomised(i));
-    fprintf('Car %d, petrol type: %d, %s \n', i, petrol_type_rng(i), petrol_type_values{i});  
+    fprintf('Car %d, petrol type: %d, %s \n', i, petrol_type_rng(i), petrol_type_values{i});
 end
 
 % initialise array to hold interarrival times
 interarrival_rng = zeros(1, number_of_cars);
 interarrival_values = zeros(1, number_of_cars);
 % fill interarrival rng array
-for i = 1:number_of_cars
+for i = 2:number_of_cars
     interarrival_rng(i) = randomised(number_of_cars + i);
     interarrival_values(i) = get_interarrival_value(randomised(number_of_cars + i));
     fprintf('Car %d, interarrival time: %d, %d\n', i, interarrival_rng(i),interarrival_values(i));
